9d43c185d6847394e98b1e2514f85987261b1e1e
ajaxthebug <hey.bhupend@gmail.com>
2023-03-16 02:56:17 +0530

    Added CTA section

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,17 @@ import './cta.css'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='gpt3__cta section__margin'>
+      <div className='gpt3__cta-heading'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button type='button'>Get Started</button>
+
+      </div>
+
+    </div>
   )
 }
 
