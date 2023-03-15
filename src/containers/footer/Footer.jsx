f85089e4e20e385a253ea0e185a1a60f37def553
ajaxthebug <hey.bhupend@gmail.com>
2023-03-16 05:19:56 +0530

    Added Blog Section

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,7 +3,14 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h2 className="gpt3__footer-heading gradient__text"></h2>
+        <div className="gpt3__footer"></div>
+        <button type="button">Request Early Access</button>
+      </div>
+      <div className=""></div>
+    </div>
   )
 }
 
