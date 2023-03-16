ab1e0758f69759cc853ad865c11305f1c4df1b67
ajaxthebug <hey.bhupend@gmail.com>
2023-03-16 07:38:29 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 // import { createRoot } from 'react-dom/client';
 
 
-import App from './App';
+import App from './app';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
